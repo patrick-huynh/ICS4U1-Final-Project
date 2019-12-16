3388c68df623c905e703f74ab5eade92852da101
The Level Three Chin <henchelsantillan@gmail.com>
2019-12-16 09:08:02 -0500

    Update Person.java
    Added javadoc comments above Person constructor

--- Prototype/src/prototype/Person.java
+++ Prototype/src/prototype/Person.java
@@ -12,6 +12,14 @@ package prototype;
 public abstract class Person {
     private String fname, lname, phone, gender, dob, address;
     private int age;
+    
+    /*Constructor for class Person.
+    /*@param fname - The first name of the Person.
+    /*@param lname - The last name of the Person.
+    /*@param phone - The phone number of the Person.
+    /*@param gender - The gender of the Person.
+    /*@param dob - The date of birth (mm/dd/yyyy) of the Person.
+    /*@param address - The address of the Person. */
     public Person (String fname, String  lname, String  phone, String gender, String dob, int age, String address) {
         this.fname = fname;
         this.lname = lname;
@@ -20,7 +28,6 @@ public abstract class Person {
         this.dob = dob;
         this.age = age;
         this.address = address;
-        
     }
     
 }
