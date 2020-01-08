01f3bcbf6021a7deeecf050b4e077fce94417e80
Patrick Huynh <58818596+patrick-huynh@users.noreply.github.com>
2020-01-08 09:59:24 -0500

    Update Activity.java

--- ICS4U Culminating/src/utility/Activity.java
+++ ICS4U Culminating/src/utility/Activity.java
@@ -1,14 +1,51 @@
 package utility;
 
+import java.time.LocalTime;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
 public class Activity {
 	private SimpleStringProperty name;
 	private SimpleBooleanProperty outdoor;
+        private LocalTime startTime, endTime;
 	
-	private Caregiver[] supervisors;
-	private Senior[] entries;
+	private Caregiver coordinator;
+	private Senior[] participants;
 	
-	
+	public Activity(String aname, boolean aoutdoor, LocalTime astartTime, LocalTime aendTime, Caregiver acoordinator, Senior aparticipants[]) {
+            name = new SimpleStringProperty();
+            name.set(aname);
+            
+            outdoor = new SimpleBooleanProperty();
+            outdoor.set(aoutdoor);
+            
+            startTime = astartTime;
+            endTime = aendTime;
+            coordinator = acoordinator;
+            participants = aparticipants;
+                              
+            
+        }
+        /**
+         * 
+         * @return 
+         */
+        public String getName(){
+            return name.get();
+        }
+        
+        public String getStartTime() {
+            return startTime.toString();
+        }
+        
+        public String getEndTime(){
+            return endTime.toString();
+            
+        }
+        
+        /*public String getDuration(){
+            //START HERE
+        }*/
+        
+        
 }
