8d9af3524db94f3f524a1418e7ecbc2a51f762ff
The Level Three Chin <henchelsantillan@gmail.com>
2020-01-12 21:54:41 -0500

    Update FoodItem.java

--- ICS4U Culminating/src/utility/FoodItem.java
+++ ICS4U Culminating/src/utility/FoodItem.java
@@ -1,14 +1,14 @@
 package utility;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import java.time.LocalDate;
 
 public class FoodItem {
 	private SimpleStringProperty name;
-	private LocalDate expiry_date;
+	private SimpleStringProperty expiryDate;
 	private FoodGroup group;
-	private SimpleIntegerProperty quantity, stock;
+	private IntegerProperty quantity, stock;
 	
 	/**Creates a FoodItem with a name, expiry date, and FoodGroup, Lists the quantity to stock numbers.
 	 * @param item_name - The name of the FoodItem.
@@ -16,11 +16,11 @@ public class FoodItem {
 	 * @param group - The FoodGroup of the FoodItem.
 	 * @param item_quantity - The quantity of products of this FoodItem currently in stock.
 	 * @param item_stock - The expected number of products of this FoodItem to be in stock.*/
-	public FoodItem(String item_name, LocalDate expiry_date, FoodGroup group, int item_quantity, int item_stock) {
+	public FoodItem(String item_name, String expiry_date, FoodGroup group, int item_quantity, int item_stock) {
 		name = new SimpleStringProperty();
 		name.set(item_name);
 		
-		this.expiry_date = expiry_date;
+		this.expiryDate.set(expiry_date);
 		
 		this.group = group;
 		
@@ -33,38 +33,43 @@ public class FoodItem {
 	
 	/**Gets the name of the FoodItem.
 	 * @return String*/
-	public String getItemName() {
+	public String getName() {
 		return name.get();
 	}
 	
 	/**Sets the name of the FoodItem.
 	 * @param item_name - String*/
-	public void setItemName(String item_name) {
+	public void setName(String item_name) {
 		name.set(item_name);
 	}
 	
 	/**Gets the String version of the LocalDate for the expiry of this FoodItem.
 	 * @return LocalDate*/
-	public LocalDate getExpiryDate() {
-		return expiry_date;
+	public String getExpiryDate() {
+		return expiryDate.get();
 	}
 	
 	/**Sets the expiry date of this FoodItem.
 	 * @param item_expiry - LocalDate*/
-	public void setExpiryDate(LocalDate expiry_date) {
-		this.expiry_date = expiry_date;
+	public void setExpiryDate(String expiry_date) {
+		this.expiryDate.set(expiry_date);
 	}
 	
 	/**Gets the FoodGroup of this FoodItem.
 	 * @return FoodGroup*/
-	public FoodGroup getFoodGroup() {
+	public FoodGroup getGroup() {
 		return group;
 	}
 	
+	/**Sets the FoodGroup of this FoodItem. The FoodGroup currently has non named Supplier.
+	 * @param name - String*/
+	public void setGroup(String name) {
+		group = new FoodGroup(name);
+	}
 	/**Sets the FoodGroup of this FoodItem.
 	 * @param name - String
 	 * @param supplier - Supplier*/
-	public void setFoodGroup(String name, Supplier supplier) {
+	public void setGroup(String name, Supplier supplier) {
 		group = new FoodGroup(name, supplier);
 	}
 	
@@ -75,9 +80,15 @@ public class FoodItem {
 	}
 	
 	/**Sets the current quantity of this FoodItem.
-	 * @param item_quantity - int*/
-	public void setQuantity(int item_quantity) {
-		quantity.set(item_quantity);
+	 * @param item_quantity - Number*/
+	public void setQuantity(Number item_quantity) {
+		quantity.set(item_quantity.intValue());
+	}
+	
+	/**Gets the quantity property for this FoodItem object.
+	 * @return IntegerProperty*/
+	public IntegerProperty quantityProperty() {
+		return quantity;
 	}
 	
 	/**Gets the expected stock of this FoodItem
@@ -87,9 +98,15 @@ public class FoodItem {
 	}
 	
 	/**Sets the expected stock of this FoodItem.
-	 * @param item_stock - int*/
-	public void setStock(int item_stock) {
-		stock.set(item_stock);
+	 * @param item_stock - Number*/
+	public void setStock(Number item_stock) {
+		stock.set(item_stock.intValue());
+	}
+	
+	/**Gets the stock property for this FoodItem object.
+	 * @return IntegerProperty*/
+	public IntegerProperty stockProperty() {
+		return stock;
 	}
 	
 	/**Checks if the current quantity of this FoodItem is below its expected stock.
