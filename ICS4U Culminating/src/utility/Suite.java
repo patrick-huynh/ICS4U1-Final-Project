e5a6ac37072c54c9dbcc8eda3057540821408c91
The Level Three Chin <henchelsantillan@gmail.com>
2020-01-15 21:50:06 -0500

    Update Suite.java

--- ICS4U Culminating/src/utility/Suite.java
+++ ICS4U Culminating/src/utility/Suite.java
@@ -1,5 +1,6 @@
 package utility;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 
@@ -11,41 +12,54 @@ public class Suite {
 	 * SINGLE_KITCHEN: One bedroom with a kitchen
 	 * DOUBLE_KITCHEN: Two bedrooms with a kitchen*/
 	public enum Type {
-		SINGLE(2), DOUBLE(4), SINGLE_KITCHEN(3), DOUBLE_KITCHEN(5);
+		SINGLE(2, "SINGLE"), DOUBLE(4, "DOUBLE"), SINGLE_KITCHEN(3, "SINGLE_KITCHEN"), 
+		DOUBLE_KITCHEN(5, "DOUBLE_KITCHEN");
 		
 		private int maximum;
+		private String typeName;
 		
 		/**Constructor for enum-type Type.
-		 * @param maximum - The maximum number of occupants permitted in the suite.*/
-		private Type(int maximum) {
+		 * @param maximum - The maximum number of occupants permitted in the suite.
+		 * @param typeName - The name of the suite style.*/
+		private Type(int maximum, String typeName) {
 			this.maximum = maximum;
+			this.typeName = typeName;
 		}
 		
 		/**Gets the value of the enum property maximum.
 		 * @return int - The maximum number of occupants permitted in the suite.*/
-		public int getMaxCapacity() {
+		public int getMaximum() {
 			return maximum;
+		}
+		
+		/**Gets the type name of this Type.*/
+		public String getTypeName() {
+			return typeName;
 		}
 	}
 	
-	private final SimpleIntegerProperty suite_number;
-	private final SimpleDoubleProperty monthly_cost;	//amount it costs to house each occupant per month
-	private final Type suite_style;
+	private final SimpleIntegerProperty suiteNumber;
+	private final SimpleDoubleProperty monthlyCost;	//amount it costs to house each occupant per month
+	private final Type suiteStyle;
+	private final SimpleStringProperty styleName;
 	private Caregiver presider;
 	private Senior[] occupants;
 	
 	/**Constructor for class Suite. Initializes an empty room with no presider.
-	 * @param number - The suite number for bookkeeping purposes.
-	 * @param cost - The monthly cost of living and maintenance of the suite.
-	 * @param suite_style - The Type of the suite.*/
-	public Suite(int number, int cost, Type suite_style) {
-		suite_number = new SimpleIntegerProperty();
-		suite_number.set(number);
+	 * @param suiteNumber - The suite number for bookkeeping purposes.
+	 * @param monthlyCost - The monthly cost of living and maintenance of the suite.
+	 * @param suiteStyle - The Type of the suite.*/
+	public Suite(int suiteNumber, int monthlyCost, Type suiteStyle) {
+		this.suiteNumber = new SimpleIntegerProperty();
+		this.suiteNumber.set(suiteNumber);
 		
-		monthly_cost = new SimpleDoubleProperty();
-		monthly_cost.set(cost);
+		this.monthlyCost = new SimpleDoubleProperty();
+		this.monthlyCost.set(monthlyCost);
 		
-		this.suite_style = suite_style;
+		this.suiteStyle = suiteStyle;
+		styleName = new SimpleStringProperty();
+		styleName.set(suiteStyle.getTypeName());
+		
 		occupants = new Senior[0];
 		presider = null;
 	}
@@ -53,13 +67,35 @@ public class Suite {
 	/**Gets the identification number of the suite.
 	 * @return int - The suite number.*/
 	public int getSuiteNumber() {
-		return suite_number.get();
+		return suiteNumber.get();
+	}
+	
+	/**Sets the identification number of the suite.
+	 * @param suiteNumber*/
+	public void setSuiteNumber(Number suiteNumber) {
+		this.suiteNumber.set(suiteNumber.intValue());
 	}
 	
 	/**Gets the configuration of the suite.
 	 * @return Type - The suite style.*/
-	public Type getType() {
-		return suite_style;
+	public Type getSuiteStyle() {
+		return suiteStyle;
+	}
+	
+	public String getStyleName() {
+		return styleName.get();
+	}
+	
+	public void setStyleName(String styleName) {
+		this.styleName.set(styleName);
+	}
+	
+	public double getMonthlyCost() {
+		return monthlyCost.get();
+	}
+	
+	public void setMonthlyCost(Number monthlyCost) {
+		this.monthlyCost.set(monthlyCost.doubleValue());
 	}
 	
 	/**Gets the presiding Caregiver of the suite.
@@ -75,9 +111,9 @@ public class Suite {
 	}
 
 	/**Adds an occupant to the suite.*/
-	public boolean addOccupant(String fname, String lname, String dob, double age, double height, double weight, String gender, 
-			String doe, long home_id, long insurance_number) {
-		if (this.suite_style.getMaxCapacity() > occupants.length) {
+	public boolean addOccupant(String fname, String lname, String dob, int age, int roomID,
+			String doe, long hID) {
+		if (this.suiteStyle.getMaximum() > occupants.length) {
 			Senior[] temp = occupants.clone();
 			occupants = new Senior[temp.length + 1];
 			
@@ -85,8 +121,7 @@ public class Suite {
 				occupants[i] = temp[i];
 			}
 			
-			occupants[occupants.length - 1] = new Senior(fname, lname, dob, age, height, weight, gender, doe, home_id, 
-					insurance_number);		
+			occupants[occupants.length - 1] = new Senior(fname, lname, dob, age, roomID, doe, hID);		
 			return true;
 			
 		} else { 
@@ -99,7 +134,7 @@ public class Suite {
 	
 	/***/
 	public boolean addOccupants(Senior...seniors) {
-		if (this.suite_style.getMaxCapacity() >= occupants.length + seniors.length) {
+		if (this.suiteStyle.getMaximum() >= occupants.length + seniors.length) {
 			Senior[] temp = occupants.clone();
 			occupants = new Senior[temp.length + seniors.length];
 			
@@ -113,7 +148,6 @@ public class Suite {
 			
 			return true;
 		}  else {
-			//write to a label or trigger a system alert stating that number of occupants exceeds
 			return false;
 		}
 	}
@@ -121,17 +155,27 @@ public class Suite {
 	/***/
 	public boolean removeOccupant(long home_id) {
 		for (int i = 0; i < occupants.length; i++) {
-			if (occupants[i] != null && occupants[i].getHomeID() == home_id) {
+			if (occupants[i] != null && occupants[i].getHID() == home_id) {
 				occupants[i] = null;
 				return true;
 			}
 		}
-		//communicate to a label or alert that no such occupant with the home id exists
 		return false;
 	}
 	
-	//Yearly cost with no aid or benefits applied. Is equal to the GROSS cost for ALL seniors
-	public double computeGrossYearlyCost() {
-		return monthly_cost.get() * occupants.length * 12;
+	public double computeWeeklyEarnings() {
+		return presider.getWage() * presider.getHours();
+	}
+	
+	public double computeMonthlyEarnings() {
+		return computeWeeklyEarnings() * 4;
+	}
+	
+	public double computeYearlyEarnings() {
+		return computeMonthlyEarnings() * 12;
+	}
+	
+	public double computeYearlyCost() {
+		return monthlyCost.get() * occupants.length * 12;
 	}
 }
