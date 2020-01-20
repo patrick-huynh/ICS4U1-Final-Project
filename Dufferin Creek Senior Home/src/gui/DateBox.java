0d691eafdc219ec924db4fe588c9e77dfef8e735
The Level Three Chin <henchelsantillan@gmail.com>
2020-01-20 09:58:50 -0500

    Update DateBox.java

--- Dufferin Creek Senior Home/src/gui/DateBox.java
+++ Dufferin Creek Senior Home/src/gui/DateBox.java
@@ -11,6 +11,7 @@ public final class DateBox {
 	private final ChoiceBox<Integer> day;
 	private final ChoiceBox<Integer> year;
 	
+	/**Creates a DateBox with filled in day and month ChoiceBox objects and an empty year ChoiceBox.*/
 	public DateBox() {
 		month = new ChoiceBox<>();
 		
@@ -27,31 +28,46 @@ public final class DateBox {
 		year = new ChoiceBox<>();
 	}
 	
+	/**Gets the ChoiceBox for month in the DateBox.
+	@return ChoiceBox<Integer>*/
 	public ChoiceBox<Integer> getMonthBox() {
 		return month;
 	}
 	
+	/**Gets the ChoiceBox for day in the DateBox.
+	@return ChoiceBox<Integer>*/
 	public ChoiceBox<Integer> getDayBox() {
 		return day;
 	}
 	
+	/**Gets the ChoiceBox for year in the DateBox.
+	@return ChoiceBox<integer>*/
 	public ChoiceBox<Integer> getYearBox() {
 		return year;
         }
 	
-	//Getters should only be run when ChoiceBox is non-null (selection has been made)
+	
+	/**Gets the current value being displayed in the month ChoiceBox.
+	@return int*/
 	public int getMonth() {
 		return month.getValue();
 	}
 	
+	/**Gets the current value being displayed in the day ChoiceBox.
+	@return int*/
 	public int getDay() {
 		return day.getValue();
 	}
 	
+	/**Gets the current value being displayed in the year ChoiceBox.
+	@return int*/
 	public int getYear() {
 		return year.getValue();
 	}
         
+	/**Sets the upper and lower year limits for the ChoiceBox for the year.
+	@param lower - The lower limit.
+	@param upper - The upper limit.*/
         public void setYearConstraints(int lower, int upper) {
             for (int i = lower; i <= upper; i++) {
                 year.getItems().add(i);
