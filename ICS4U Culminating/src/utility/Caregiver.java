d32ed02e1ba8349f5dd021bd4513a983b098a50a
The Level Three Chin <henchelsantillan@gmail.com>
2020-01-19 22:14:01 -0500

    Update Caregiver.java

--- ICS4U Culminating/src/utility/Caregiver.java
+++ ICS4U Culminating/src/utility/Caregiver.java
@@ -1,226 +1,137 @@
 package utility;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import java.util.List;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
 
-public class Suite {
-	
-	/**Enum-type with typesafe constants for suite style.
-	 * SINGLE: One bedroom
-	 * DOUBLE: Two bedrooms
-	 * SINGLE_KITCHEN: One bedroom with a kitchen
-	 * DOUBLE_KITCHEN: Two bedrooms with a kitchen*/
-	public enum Type {
-		SINGLE(2, "SINGLE", 1500, 15), DOUBLE(4, "DOUBLE", 3000, 30), SINGLE_KITCHEN(3, "SINGLE KITCHEN", 3000, 30), 
-		DOUBLE_KITCHEN(5, "DOUBLE KITCHEN", 5000, 50);
-		
-		private int maximum;
-		private String typeName;
-		private double monthlyCost, feeMultiple;
-		
-		/**Constructor for enum-type Type.
-		 * @param maximum - The maximum number of occupants permitted in the suite.
-		 * @param typeName - The name of the suite style.*/
-		private Type(int maximum, String typeName, double monthlyCost, double feeMultiple) {
-			this.maximum = maximum;
-			this.typeName = typeName;
-			this.monthlyCost = monthlyCost;
-			this.feeMultiple = feeMultiple;
-		}
-		
-		/**Gets the value of the enum property maximum.
-		 * @return int - The maximum number of occupants permitted in the suite.*/
-		public int getMaximum() {
-			return maximum;
-		}
-		
-		/**Gets the type name of this Type.*/
-		public String getTypeName() {
-			return typeName;
-		}
-		
-		public double getMonthlyCost() {
-			return monthlyCost;
-		}
-		
-		public double getFeeMultiple() {
-			return feeMultiple;
-		}
-	}
-	
-	private IntegerProperty numberOfOccupants;
-	private final SimpleIntegerProperty suiteNumber;
-	private final Type suiteStyle;
-	private final SimpleStringProperty styleName;
-	private Caregiver presider;
-	private Senior[] occupants;
-	
-	/**Constructor for class Suite. Initializes an empty room with no presider.
-	 * @param suiteNumber - The suite number for bookkeeping purposes.
-	 * @param monthlyCost - The monthly cost of living and maintenance of the suite.
-	 * @param suiteStyle - The Type of the suite.*/
-	public Suite(int suiteNumber, Type suiteStyle) {
-		this.suiteNumber = new SimpleIntegerProperty();
-		this.suiteNumber.set(suiteNumber);
-		
-		this.suiteStyle = suiteStyle;
-		styleName = new SimpleStringProperty();
-		styleName.set(suiteStyle.getTypeName());
-		
-		occupants = new Senior[0];
-                numberOfOccupants = new SimpleIntegerProperty();
-                numberOfOccupants.set(0);
-		presider = null;
-	}
-	
-	public IntegerProperty numberOfOccupantsProperty() {
-		return numberOfOccupants;
-	}
-	
-	/**Gets the identification number of the suite.
-	 * @return int - The suite number.*/
-	public int getSuiteNumber() {
-		return suiteNumber.get();
-	}
-	
-	/**Sets the identification number of the suite.
-	 * @param suiteNumber*/
-	public void setSuiteNumber(Number suiteNumber) {
-		this.suiteNumber.set(suiteNumber.intValue());
-	}
-	
-	/**Gets the configuration of the suite.
-	 * @return Type - The suite style.*/
-	public Type getSuiteStyle() {
-		return suiteStyle;
-	}
-	
-	public String getStyleName() {
-		return styleName.get();
-	}
-	
-	public void setStyleName(String styleName) {
-		this.styleName.set(styleName);
-	}
-	
-	/**Gets the presiding Caregiver of the suite.
-	 * @return presider - The presider responsible for the Seniors in the suite.*/
-	public Caregiver getPresider() {
-		return presider;
-	}
-	
-	/**Sets the presiding Caregiver of the suite.
-	 * @param presider - The Caregiver assigned to the suite.*/
-	public void setPresider(Caregiver presider) {
-		this.presider = presider;
-		presider.assigned = true;
-	}
-	
-	public boolean occupantExists(Senior senior) {
-		for (int i = 0; i < occupants.length; i++) {
-			if (occupants[i].getHID() == senior.getHID()) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean presiderExists(Caregiver caregiver) {
-		if (this.getPresider() != null) {
-			return true;
-		}
-		return false;
-	}
+public class Caregiver extends Person {
 
-	/**Adds an occupant to the suite.*/
-	public boolean addOccupant(String fname, String lname, String dob, int age, int roomID,
-			double hours, long hID) {
-		if (this.suiteStyle.getMaximum() > occupants.length) {
-			Senior[] temp = occupants.clone();
-			occupants = new Senior[temp.length + 1];
-			
-			for (int i = 0; i < temp.length; i++) {
-				occupants[i] = temp[i];
-			}
-			
-			Senior senior = new Senior(fname, lname, dob, age, roomID, hours, hID);
-			occupants[occupants.length - 1] = senior;
-            numberOfOccupants.set(numberOfOccupants.get() + 1);
-            senior.inside = true;
+    private final SimpleLongProperty empNum;
+    private final SimpleDoubleProperty wage, hours, weekly_pay, monthly_pay, annual_pay;
+    public boolean assigned;
+    
+    /**
+     * Constructor for class Caregiver.
+     * @param empNum - The employee number.
+	 * @param hours - The hours worked per week.
+	 * @param wage - The hourly wage.	
+     */
+    public Caregiver(String fname, String lname, String DOB, int age, int roomID,
+            long empNum, double hours, double wage) {
+        super(fname, lname, DOB, age, roomID);
+
+        this.empNum = new SimpleLongProperty();
+        this.empNum.set(empNum);
+
+        this.hours = new SimpleDoubleProperty();
+        this.hours.set(hours);
+
+        this.wage = new SimpleDoubleProperty();
+        this.wage.set(wage);
+        
+        weekly_pay = new SimpleDoubleProperty();
+        weekly_pay.set(0);
+
+        monthly_pay = new SimpleDoubleProperty();
+
+        annual_pay = new SimpleDoubleProperty();
+        assigned = false;
+    }
+
+    /**Gets the employee number of this Caregiver.
+     * @return long*/
+    public long getEmpNum() {
+        return empNum.get();
+    }
+
+    /**Sets the employee number of this Caregiver.
+     * @param empNum*/
+    public void setEmpNum(Number empNum) {
+        this.empNum.set(empNum.longValue());
+    }
+    
+    /**Gets the number of weekly hours worked by this Caregiver.
+     * @return double*/
+    public double getHours() {
+    	return hours.get();
+    }
+    
+    /**Sets the number of weekly hours worked by this Caregiver.
+     * @param hours*/
+    public void setHours(Number hours) {
+    	this.hours.set(hours.doubleValue());
+    }
+
+    /**Gets the hourly wage of this Caregiver.
+     * @return double*/
+    public double getWage() {
+    	return wage.get();
+    }
+    
+    /**Sets the hourly wage of this Caregiver.
+     * @param wage*/
+    public void setWage(Number wage) {
+    	this.wage.set(wage.doubleValue());
+    }
+    
+    public void calculateWeeklyPay() {
+    	weekly_pay.set(wage.get()*hours.get()*0.735);
+    }
+    public double getWeeklyPay() {
+    	return weekly_pay.get();
+    }
+    public void calculateMonthlyPay() {
+    	monthly_pay.set(wage.get()*hours.get()*0.735*4.34);
+    }
+    public double getMonthlyPay() {
+    	return monthly_pay.get();
+    }
+    public void calculateAnnualPay() {
+    	annual_pay.set(wage.get()*hours.get()*0.735*4.34*12.0);
+    }
+    public double getAnnualPay() {
+    	return annual_pay.get();
+    }
+    
+    public static void sortEmpNum(List<Caregiver> c){
+        for (int i = 0; i < c.size(); i++) {
+    		Caregiver holder = c.get(i);
+    		int j = i - 1;
+    		while (j >- 0 && c.get(j).getEmpNum() > holder.getEmpNum()) {
+    			c.set(j + 1, c.get(j));
+    			j--;
+    		}
+    		c.set(j + 1, holder);
+    	}
+    }
+    
+    public static Caregiver searchEmpNum(long empNum, List<Caregiver> c, int l, int r) {
+    	Caregiver.sortEmpNum(c);
+        l = 0;
+        r = c.size() - 1;
+        
+        while (l<=r) {
+            int mid = (l+r)/2;
+            if (empNum == c.get(mid).getEmpNum()) {
+                return c.get(mid);
+            } else if (empNum > c.get(mid).getEmpNum()) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
             
-			System.out.println("Senior added successfully.");
-			return true;
-			
-		} else { 
-			System.out.println("Senior add failed. This suite has reached maximum occupants.");
-			return false;
-		}
-	}
-	
-	public boolean addOccupant(Senior senior) {
-		if (this.suiteStyle.getMaximum() > occupants.length) {
-			Senior[] temp = occupants.clone();
-			occupants = new Senior[temp.length + 1];
-			
-			for (int i = 0; i < temp.length; i++) {
-				occupants[i] = temp[i];
-			}
-			
-			occupants[occupants.length - 1] = senior;
-                        numberOfOccupants.set(numberOfOccupants.get() + 1);
-			System.out.println("Senior added successfully.");
-			return true;
-		} else {
-			System.out.println("Senior add failed. This suite has reached maximum occupants.");
-			return false;
-		}
-	}
-	
-	/***/
-	public boolean addOccupants(Senior...seniors) {
-		if (this.suiteStyle.getMaximum() >= occupants.length + seniors.length) {
-			Senior[] temp = occupants.clone();
-			occupants = new Senior[temp.length + seniors.length];
-			
-			for (int i = 0; i < temp.length; i++) {
-				occupants[i] = temp[i];
-			}
-			
-			for (int i = 0; i < seniors.length; i++) {
-				occupants[temp.length + i] = seniors[i];
-			}
-			
-                        numberOfOccupants.set(numberOfOccupants.get() + seniors.length);
-			return true;
-		}  else {
-			return false;
-		}
-	}
-	
-	/***/
-	public boolean removeOccupant(long home_id) {
-		for (int i = 0; i < occupants.length; i++) {
-			if (occupants[i] != null && occupants[i].getHID() == home_id) {
-				occupants[i] = null;
-                numberOfOccupants.set(numberOfOccupants.get() - 1);
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public boolean homeIDExists(long homeid) {
-		if (occupants.length > 0 && Senior.searchHomeID(homeid, occupants, 0, occupants.length) != null) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean empNumExists(long empNum) {
-		if (presider.getEmpNum() == empNum) {
-			return true;
-		} 
-		return false;
-	}
+        }
+        return null;
+             
+    }
+    
+            @Override
+    /**Returns a String representation of this Caregiver.
+     * @return String*/
+    public String toString() {
+        return super.toString() + "\n"
+                + "Employee Number: " + getEmpNum() + "\n"
+                + "Hours Per Week: " + getHours() + "\n"
+                + "Wage: " + getWage() + "\n";
+    }
 }
