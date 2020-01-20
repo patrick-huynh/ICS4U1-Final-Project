cd5bb615d98ac6145f0fbb6230aedc2f67f2dd5a
The Level Three Chin <henchelsantillan@gmail.com>
2020-01-19 22:12:34 -0500

    Update Activity.java

--- ICS4U Culminating/src/utility/Activity.java
+++ ICS4U Culminating/src/utility/Activity.java
@@ -1,229 +1,137 @@
 package utility;
 
-import java.time.Duration;
-import java.time.*;
-import java.time.format.*;
-import static java.time.temporal.ChronoField.HOUR_OF_DAY;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import java.text.SimpleDateFormat;
-import static java.time.temporal.ChronoField.MINUTE_OF_DAY;
-import java.util.ArrayList;
 import java.util.List;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleLongProperty;
 
-public class Activity {
+public class Caregiver extends Person {
 
-    private SimpleStringProperty name;
-    private SimpleBooleanProperty outdoor;
-    private LocalTime startTime, endTime;
-    private Caregiver coordinator;
-    private Senior[] participants;
-
+    private final SimpleLongProperty empNum;
+    private final SimpleDoubleProperty wage, hours, weekly_pay, monthly_pay, annual_pay;
+    public boolean assigned;
+    
     /**
-     *
-     * @param aname - The name of the activity.
-     * @param aoutdoor - Whether the activity is hosted outdoors, otherwise
-     * indoors.
-     * @param astartTime - The start time of the activity.
-     * @param aendTime - The end time of the activity.
+     * Constructor for class Caregiver.
+     * @param empNum - The employee number.
+	 * @param hours - The hours worked per week.
+	 * @param wage - The hourly wage.	
      */
-    public Activity(String aname, boolean aoutdoor, LocalTime astartTime, LocalTime aendTime) {
-        name = new SimpleStringProperty();
-        name.set(aname);
+    public Caregiver(String fname, String lname, String DOB, int age, int roomID,
+            long empNum, double hours, double wage) {
+        super(fname, lname, DOB, age, roomID);
 
-        outdoor = new SimpleBooleanProperty();
-        outdoor.set(aoutdoor);
+        this.empNum = new SimpleLongProperty();
+        this.empNum.set(empNum);
 
-        startTime = astartTime;
-        endTime = aendTime;
+        this.hours = new SimpleDoubleProperty();
+        this.hours.set(hours);
 
+        this.wage = new SimpleDoubleProperty();
+        this.wage.set(wage);
+        
+        weekly_pay = new SimpleDoubleProperty();
+        weekly_pay.set(0);
+
+        monthly_pay = new SimpleDoubleProperty();
+
+        annual_pay = new SimpleDoubleProperty();
+        assigned = false;
     }
 
-    /**
-     * @return String - Name of the activity.
-     */
-    public String getName() {
-        return name.get();
+    /**Gets the employee number of this Caregiver.
+     * @return long*/
+    public long getEmpNum() {
+        return empNum.get();
     }
 
-    /**
-     * @return String - Start time of the activity (24-hour clock.
-     */
-    public String getStartTime() {
-        return startTime.toString();
+    /**Sets the employee number of this Caregiver.
+     * @param empNum*/
+    public void setEmpNum(Number empNum) {
+        this.empNum.set(empNum.longValue());
+    }
+    
+    /**Gets the number of weekly hours worked by this Caregiver.
+     * @return double*/
+    public double getHours() {
+    	return hours.get();
+    }
+    
+    /**Sets the number of weekly hours worked by this Caregiver.
+     * @param hours*/
+    public void setHours(Number hours) {
+    	this.hours.set(hours.doubleValue());
     }
 
-    /**
-     * @return String - End time of the activity (24-hour clock).
-     */
-    public String getEndTime() {
-        return endTime.toString();
-
+    /**Gets the hourly wage of this Caregiver.
+     * @return double*/
+    public double getWage() {
+    	return wage.get();
     }
-
-    /**
-     * Takes the difference between the endTime and the startTime in order to
-     * determine the duration.
-     *
-     * @return double - How long the activity will take in hours.
-     */
-    public double getDuration() {
-        Duration duration = Duration.between(endTime, startTime);
-        return Math.abs(duration.toMinutes()/60.0);
+    
+    /**Sets the hourly wage of this Caregiver.
+     * @param wage*/
+    public void setWage(Number wage) {
+    	this.wage.set(wage.doubleValue());
     }
-
-    /**
-     * @return Caregiver - The coordinator of the activity, usually a caregiver.
-     */
-    public Caregiver getCoordinator() {
-        return coordinator;
-
+    
+    public void calculateWeeklyPay() {
+    	weekly_pay.set(wage.get()*hours.get()*0.735);
     }
-
-    /**
-     * @return Senior[] - The seniors participating in the activity.
-     */
-    public Senior[] getParticipants() {
-        return participants;
+    public double getWeeklyPay() {
+    	return weekly_pay.get();
     }
-
-    /**
-     * @param aname - The name of the activity
-     */
-    public void setName(String aname) {
-        name.set(aname);
+    public void calculateMonthlyPay() {
+    	monthly_pay.set(wage.get()*hours.get()*0.735*4.34);
     }
-
-    /**
-     * @param start - Start time of the activity.
-     */
-    public void setStartTime(LocalTime start) {
-        startTime = start;
+    public double getMonthlyPay() {
+    	return monthly_pay.get();
     }
-
-    /**
-     * @param end - End time of the activity
-     */
-    public void setEndTime(LocalTime end) {
-        endTime = end;
+    public void calculateAnnualPay() {
+    	annual_pay.set(wage.get()*hours.get()*0.735*4.34*12.0);
     }
-
-    /**
-     * @param coordinator - The caregiver that is hosting the activity.
-     */
-    public void setCoordinator(Caregiver coordinator) {
-        this.coordinator = coordinator;
+    public double getAnnualPay() {
+    	return annual_pay.get();
     }
-
-    /**
-     * @param participants - The array of Senior that are all participating in
-     * this activity.
-     */
-    public void setParticipants(Senior[] participants) {
-        this.participants = participants;
+    
+    public static void sortEmpNum(List<Caregiver> c){
+        for (int i = 0; i < c.size(); i++) {
+    		Caregiver holder = c.get(i);
+    		int j = i - 1;
+    		while (j >- 0 && c.get(j).getEmpNum() > holder.getEmpNum()) {
+    			c.set(j + 1, c.get(j));
+    			j--;
+    		}
+    		c.set(j + 1, holder);
+    	}
     }
-
-    /**
-     * @return boolean - Returns true if the activity is hosted outdoors.
-     */
-    public boolean isOutDoor() {
-        return outdoor.get();
-    }
-
-    /**
-     * This method sorts the list of activities by time (sorts from most closest
-     * to most furthest.)
-     *
-     * @param activities - Array of unsorted activities.
-     */
-    public static void sortTime(Activity[] activities) {
-        for (int i = 0; i < activities.length; i++) {
-            Activity holder = activities[i];
-            int j = i - 1;
-            while ((j >= 0) && activities[j].startTime.get(MINUTE_OF_DAY) > holder.startTime.get(MINUTE_OF_DAY)) {
-                activities[j + 1] = activities[j];
-                j--;
+    
+    public static Caregiver searchEmpNum(long empNum, List<Caregiver> c, int l, int r) {
+    	Caregiver.sortEmpNum(c);
+        l = 0;
+        r = c.size() - 1;
+        
+        while (l<=r) {
+            int mid = (l+r)/2;
+            if (empNum == c.get(mid).getEmpNum()) {
+                return c.get(mid);
+            } else if (empNum > c.get(mid).getEmpNum()) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
-            activities[j + 1] = holder;
-        }
-    }
-
-    /**
-     * Sorts the activity names in alphabetical order.
-     * @param activities - The array of activities.
-     */
-    public static void sortAlpha(Activity[] activities) {
-        for (int i = 0; i < activities.length; i++) {
-            Activity holder = activities[i];
-            int j = i - 1;
-            while ((j >= 0) && activities[j].getName().charAt(0) > holder.getName().charAt(0)) {
-                activities[j + 1] = activities[j];
-                j--;
-            }
-            activities[j + 1] = holder;
-        }
-    }
-
-    /**
-     * Searches for an activity within an array of activity by the name. If no
-     * activity is found with that name, will return null.
-     *
-     * @param name - Name of the activity.
-     * @param activities - Array of activities.
-     * @return
-     */
-    public static Activity findByName(String name, Activity[] activities) {
-        for (int i = 0; i < activities.length; i++) {
-            if (name.equalsIgnoreCase(activities[i].getName())) {
-                return activities[i];
-            }
+            
         }
         return null;
+             
     }
-    /**
-     * 
-     * @param numOfActivities - The number of random activities you want to generate.
-     * @return List<> - The list of activities put into a list of class Activity.
-     */
-    public static List<Activity> randomGen(int numOfActivities) {
-        String[] names = {"Soccer", "Basketball", "Stretching", "Bingo", "Poker", "Speed Walking", "Flag Football", "Movies"};
-        Boolean[] isOutdoor = {true, false, true, false, false, true, true, false};
-        String[] startTime = {"06:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00"};
-        List<Activity> a = new ArrayList<>();
-        // Duration in minutes
-
-        long[] duration = {30, 60, 90, 120};
-        for (int i = 0; i < numOfActivities; i++) {
-            int namesIndex = (int) (Math.random() * (names.length));
-            int startTimeIndex = (int) (Math.random() * (startTime.length));
-            int durationIndex = (int) (Math.random() * (duration.length));
-            LocalTime start = LocalTime.parse(startTime[startTimeIndex]);
-            LocalTime end = start.plusMinutes(duration[durationIndex]);
-
-            a.add(new Activity(names[namesIndex], isOutdoor[namesIndex], start, end));
-
-        }
-        return a;
-    }
-
-    /**
-     * Prints the properties of the Activity class in a neat format.
-     * @return String - The properties.
-     */
+    
+            @Override
+    /**Returns a String representation of this Caregiver.
+     * @return String*/
     public String toString() {
-        String type;
-        if (isOutDoor()) {
-            type = "Outdoor";
-        } else {
-            type = "Indoor";
-        }
-
-        return "Name: " + getName() + "\n"
-                + "Start Time: " + getStartTime() + "\n"
-                + "End Time: " + getEndTime() + "\n"
-                + "Duration: " + getDuration() + "\n"
-                + "Type: " + type + "\n";
-
+        return super.toString() + "\n"
+                + "Employee Number: " + getEmpNum() + "\n"
+                + "Hours Per Week: " + getHours() + "\n"
+                + "Wage: " + getWage() + "\n";
     }
-
 }
